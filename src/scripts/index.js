1359dc9577610bfa63238fefaff4217a7b91778e
motokikasai <kmomoky@hotmail.com>
2020-02-21 11:15:17 +0100

    Create Bootstrap carousel with a hack by disabling html loader inside wp config file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
-
+import "bootstrap";
+import "bootstrap-scss";
 
 // \/ All of your javascript should go here \/
-
